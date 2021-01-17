2d9442a48f5b23e9b1af3b1a364c723be7a71ae0
RhinoLee <picklerhino@gmail.com>
2021-01-17 11:09:57 +0800

    draggable lists

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -11,8 +11,11 @@ import 'styles'
 
 
 // Vue.js
+import Rails from '@rails/ujs';
+import draggable from 'vuedraggable';
 import Vue from 'vue/dist/vue.esm';
 import List from 'components/list';
+
 
 
 document.addEventListener('turbolinks:load', function(e){
@@ -23,11 +26,33 @@ document.addEventListener('turbolinks:load', function(e){
     new Vue({
       components: {
         List,
+        draggable
       },
       el,
       data: {
         lists: JSON.parse(el.dataset.lists)
-      }
+      },
+      methods: {
+        listMoved(e){
+          console.log(e);
+
+          let data = new FormData();
+          data.append("list[position]", e.moved.newIndex + 1)
+
+          Rails.ajax({
+            url: `/lists/${ e.moved.element.id }/move`,
+            type: 'PUT',
+            data,
+            dataType: 'json',
+            success: res => {
+              console.log(res);
+            },
+            error: err => {
+              console.log(err);
+            }
+          })
+        }
+      },
 
     })
   }
